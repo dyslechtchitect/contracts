3cf10bf6fa8adc53d9a4ce967abd95a0f1f56fa4
ron <ron.neuman@gmail.com>
2024-02-14 21:16:55 +0200

    changed ride to contract

--- lambda/list/index.js
+++ lambda/list/index.js
@@ -1,69 +1,100 @@
-const { randomUUID } = require('crypto');
-const AWS  = require('aws-sdk');
+const randomBytes = require('crypto').randomBytes;
+const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = async (event, context) => {
-    try {
-        if (!event.requestContext.authorizer) {
-            return errorResponse('Authorization not configured', context.awsRequestId);
-        }
 
-        const userId = event.requestContext.authorizer.claims['sub'];
-        const contractId = randomUUID();
+exports.handler = (event, context, callback) => {
 
-        console.log('Received context', context);
-        console.log('Received event', event);
-        console.log('Received contractId', contractId);
-        console.log('Received userId', userId);
+    if (!event.requestContext.authorizer) {
+      errorResponse('Authorization not configured', context.awsRequestId, callback);
+      return;
+    }
+    
+    console.log('Received path params', event.pathParameters);
+    console.log('Received query params', event.queryStringParameters);
+    console.log('Received context', context);
+    console.log('Received event', event);
+    
+    const userId = event.requestContext.authorizer.claims?.sub;
+    const path = event.path;
 
-        const username = event.requestContext.authorizer.claims['cognito:username'];
-        const requestBody = JSON.parse(event.body);
+    const contractId = event.queryStringParameters?.contractId ?? event.pathParameters?.contractId ;
+    
 
-        await createContract(userId, contractId, username, requestBody);
-
-        return {
-            statusCode: 201,
-            body: JSON.stringify({
-                ContractId: contractId,
-                Contract: requestBody,
-                userId: userId,
-                username: username,
-                createdDate: new Date().toISOString(),
-                updatedDate: new Date().toISOString(),
-            }),
+    const username = event.requestContext.authorizer.claims['cognito:username'];
+    console.log('Received contractId', contractId);
+    console.log('Received userId', userId);
+    if (!!contractId) {
+    fetchContract(userId, contractId).then((result, error) => {
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(
+                [result.Item]
+            ),
             headers: {
                 'Access-Control-Allow-Origin': '*',
             },
-        };
-    } catch (error) {
-        console.error(error);
-        return errorResponse(error.message, context.awsRequestId);
+        });
+    }).catch((err) => {
+        console.error(err);
+
+        errorResponse(err, context.awsRequestId, callback)
+    });
+    } else {
+        listUnicorns(userId).then((result, error) => {
+              console.log('Received result', result);
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(
+                result
+            ),
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+    }).catch((err) => {
+        console.error(err);
+
+        errorResponse(err.message, context.awsRequestId, callback)
+    });
     }
 };
 
-async function createContract(userId, contractId, username, body) {
-    await ddb.put({
-        TableName: 'contract',
-        Item: {
-            id: contractId,
+
+function fetchContract(userId, contractId) {
+    return ddb.get({
+        TableName: 'contracts',
+        Key: {
             userId: userId,
-            username: username,
-            body: body,
-            createdDate: new Date().toISOString(),
-            updatedDate: new Date().toISOString(),
+            contractId: contractId
         },
     }).promise();
 }
 
-function errorResponse(errorMessage, awsRequestId) {
-    return {
-        statusCode: 500,
-        body: JSON.stringify({
-            Error: errorMessage,
-            Reference: awsRequestId,
-        }),
-        headers: {
-            'Access-Control-Allow-Origin': '*',
+function listUnicorns(userId) {
+   const queryParams = {
+        TableName: 'contracts',
+        KeyConditionExpression: `userId = :id`,
+        ExpressionAttributeValues: {
+            ':id': userId,
         },
     };
+    const response = ddb.query(queryParams)
+    console.log(response.toString)
+  
+    return response.promise();
+}
+
+
+function errorResponse(errorMessage, awsRequestId, callback) {
+  callback(null, {
+    statusCode: 500,
+    body: JSON.stringify({
+      Error: errorMessage,
+      Reference: awsRequestId,
+    }),
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  });
 }
