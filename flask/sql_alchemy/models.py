9f6d4af8ab738d2d2dc1f0d7bcc709b7ad3088fe
ron <ron.neuman@gmail.com>
2024-03-01 09:18:35 +0200

    UUID types for mysql

--- flask/sql_alchemy/models.py
+++ flask/sql_alchemy/models.py
@@ -33,9 +33,9 @@ class User(Base):
 
 class Address(Base):
     __tablename__ = "address"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id = Column(UUID(), primary_key=True)
     email_address: Mapped[str]
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user_id = Column(UUID(), ForeignKey("user.id"))
     user: Mapped["User"] = relationship(back_populates="addresses")
     def __repr__(self) -> str:
         return f"Address(id={self.id!r}, email_address={self.email_address!r})"
