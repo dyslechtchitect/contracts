1f8e4da61bcdc5e5908188319269d111f8c83f72
ron <ron.neuman@gmail.com>
2024-03-09 14:24:03 +0200

    trying to test stuf

--- backend/database_gateway/run_server.py
+++ backend/database_gateway/run_server.py
@@ -15,19 +15,27 @@ from db.crud.crud import CRUD
 from db.models import Base
 from server import ContractsServer
 
-boto_client: BaseClient = boto3.client('cognito-idp', Config.AWS_REGION)
-app: Flask = Flask(__name__)
-app.config.from_object(Config)
-cognito_lib_auth = CognitoLibAuth(app)
-flask_coginto = CognitoAuth(app)
 
-engine = create_engine(f'sqlite:///{Config.DB_NAME}.db')  # connect to server
-Base.metadata.create_all(engine)
-crud = CRUD(engine)
-db_adapter = DbAdapter(crud)
-boto_adapter = BotoAdapter(boto_client)
-contracts_app = ContractsApp(db_adapter, boto_adapter)
-server = ContractsServer(app, contracts_app)
+class Boiler:
+    boto_client: BaseClient = boto3.client('cognito-idp', Config.AWS_REGION)
+    app: Flask = Flask(__name__)
+    app.config.from_object(Config)
+    with app.app_context():
+        cognito_lib_auth = CognitoLibAuth(app)
+        flask_coginto = CognitoAuth(app)
+
+        engine = create_engine(f'sqlite:///{Config.DB_NAME}.db')  # connect to server
+        Base.metadata.create_all(engine)
+        crud = CRUD(engine)
+        db_adapter = DbAdapter(crud)
+        boto_adapter = BotoAdapter(boto_client)
+        contracts_app = ContractsApp(db_adapter, boto_adapter)
+        server = ContractsServer(app, contracts_app)
+
 
 if __name__ == '__main__':
-    server.app.run()
+    Boiler.app.run()
+
+
+def create_app():
+    return Boiler.app
--- backend/database_gateway/tests/test_server.py
+++ backend/database_gateway/tests/test_server.py
@@ -1,14 +1,34 @@
+import unittest
+from functools import wraps
+from importlib import reload
+from unittest.mock import patch
+
 import pytest
+
+import contracts_app
 import run_server
+import server
+from contracts_app import ContractsApp
+
+
+def setUpModule():
+    """
+    Patches out the decorator (in the library) and reloads the module
+    under test.
+    """
+    patch('flask_cognito.cognito_auth_required', mock_cognito_auth_required).start()
+    reload(server)
 
 
 @pytest.fixture()
 def app():
-    app = run_server.app
-    app.config.update({
-        "TESTING": True,
-    })
-    yield app
+    with run_server.Boiler.app.app_context():
+        app_ = run_server.Boiler.app
+        app_.config.update({
+            "TESTING": True,
+        })
+
+    yield app_
 
 
 @pytest.fixture()
@@ -20,6 +40,31 @@ def client(app):
 def runner(app):
     return app.test_cli_runner()
 
-def test_request_example(client):
-    response = client.post("/contract")
-    assert b"<h2>Hello, World!</h2>" in response.data
+
+patch('server.current_cognito_jwt')
+
+
+def mock_cognito_auth_required(fn):
+    """
+    This is a dummy wrapper of @cognito auth required, it passes-through
+    to the
+    wrapped fuction without performing any cognito logic.
+
+    """
+
+    @wraps(fn)
+    def decorator(*args, **kwargs):
+        return fn(*args, **kwargs)
+
+    return decorator
+
+
+@patch('server.current_cognito_jwt')
+class TestContractApp(unittest.TestCase):
+    def test_my_example_function(self, mock_current_cognito_jwt):
+        mock_current_cognito_jwt.__getitem__.return_value = 'test'
+        response = server.ContractsServer.create_user()
+        self.assertEqual(response, {'your_sub': 'test'})
+
+
+
