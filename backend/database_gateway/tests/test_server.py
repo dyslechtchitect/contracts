eb7dd08b304ce422e846df8d40cb29c89e8b2a0a
ron <ron.neuman@gmail.com>
2024-03-09 22:29:38 +0200

    moved to server utils

--- backend/database_gateway/tests/test_server.py
+++ backend/database_gateway/tests/test_server.py
@@ -1,20 +1,17 @@
 import unittest
-import uuid
-from unittest.mock import patch, MagicMock
-from datetime import datetime
-import json
 
-from flask import Flask
 from sqlalchemy import create_engine
-from werkzeug.datastructures import ImmutableMultiDict
 
 from contracts_app import ContractsApp
 from server import ContractsServer
 from adapters.boto_adapter import BotoAdapter
 from adapters.db_adapter import DbAdapter
-from dto import UserDto
-from db.models import Base, User
+from db.models import Base
 from db.crud.crud import CRUD
+from flask import Flask
+from unittest.mock import MagicMock
+
+from tests.test_utils import with_server_context
 
 
 class TestCreateUser(unittest.TestCase):
@@ -46,26 +43,18 @@ class TestCreateUser(unittest.TestCase):
         # Create the ContractsServer instance
         self.server = ContractsServer(self.app, self.contracts_app)
 
-    def test_create_user(self):
-        # Mock authentication decorator
-        auth_decorator = MagicMock()
-        auth_decorator.return_value = lambda func: func
-        expected_user_id = str(uuid.uuid4())
-        with patch('server.current_cognito_jwt', {'sub': expected_user_id, 'username': 'test_username'}), \
-             self.app.test_request_context():
-            # Mock cognito_auth extension and add it to the Flask application context
-            self.app.extensions = {'cognito_auth': MagicMock(get_token=MagicMock(return_value="mock_token"))}
+    @with_server_context
+    def test_create_user(self, expected_user_id):
+        # Make a POST request to create a user
+        response = self.client.post('/user')
 
-            with patch('server.cognito_auth_required', auth_decorator):
-                # Make a POST request to create a user
-                response = self.client.post('/user')
+        # Assert that the BotoAdapter's get_user_data method is called with the correct username
+        self.boto_adapter.get_user_data.assert_called_once_with('test_username')
 
-                # Assert that the BotoAdapter's get_user_data method is called with the correct username
-                self.boto_adapter.get_user_data.assert_called_once_with('test_username')
-
-                # Assert the status code and response data
-                self.assertEqual(response.status_code, 200)
-                self.assertEqual(expected_user_id, str(response.data.decode()))
+        # Assert the status code of the response
+        self.assertEqual(response.status_code, 200)
+        # Assert that the response data matches the expected user ID
+        self.assertEqual(expected_user_id, response.data.decode())
 
 
     def tearDown(self):
